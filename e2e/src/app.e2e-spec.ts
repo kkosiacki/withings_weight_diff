787281cb082fe8844d9ca56f468e609420e6a235
Karol Kosiacki <karol.kosiacki@gmail.com>
2022-02-06 21:35:30 +0100

    Change app name in config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('WithingsWeightDiff app is running!');
+    expect(page.getTitleText()).toEqual('withings-weight-diff app is running!');
   });
 
   afterEach(async () => {
